c6a259146bed4815d4f118f10d67a339437639e3
Isaac Alencar <alencarisaac3@gmail.com>
2021-06-11 20:37:39 -0300

    Initial commit

---
+++ todo_list_app/lib/main.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+import "package:todo_list_app/home.dart";
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Home(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
